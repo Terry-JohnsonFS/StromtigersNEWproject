68259a9126a45c2b0ceed04c4dcd5137776bea08
Terry-JohnsonFS <97139342+Terry-JohnsonFS@users.noreply.github.com>
2022-11-09 19:17:14 -0800

    app fixed and now has running screens only thing added is plus buttons next to toggle buttons

--- app/src/main/java/com/example/testapplication/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/testapplication/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("Manage your daily tasks");
     }
 
     public LiveData<String> getText() {
