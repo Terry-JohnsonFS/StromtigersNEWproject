20d9695114c8af57a5feb4e89a2ea9a0f170cf7b
Terry-JohnsonFS <97139342+Terry-JohnsonFS@users.noreply.github.com>
2022-11-29 17:36:10 -0800

    progress bars now can have a custom amount of hours set to them in the create goal area (uses shared pref to grab that int)

--- app/src/main/java/com/example/testapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/testapplication/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,8 @@
 package com.example.testapplication.ui.dashboard;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +29,15 @@ public class DashboardFragment extends Fragment {
     ProgressBar progressBar3;
     Button btn7;
     Button BtnBAck;
+    int progbar1;
+    int progbar2;
+    int progbar3;
+
+    SharedPreferences sharedPreferences2;
+    public static final String mypreference2 = "mypref";
+    public static final String HoursG1 = "HoursGoal1Key";
+    public static final String HoursG2 = "HoursGoal2Key";
+    public static final String HoursG3 = "HoursGoal3Key";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,9 +50,22 @@ public class DashboardFragment extends Fragment {
         final TextView textView = binding.editTextTextPersonName;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
+        sharedPreferences2 = getContext().getSharedPreferences(mypreference2, Context.MODE_PRIVATE);
+        String sprogressbar1 = sharedPreferences2.getString(HoursG1, "");
+        String sprogressbar2 = sharedPreferences2.getString(HoursG2, "");
+        String sprogressbar3 = sharedPreferences2.getString(HoursG3, "");
+        progbar1 = Integer.parseInt(sprogressbar1);
+        progbar2 = Integer.parseInt(sprogressbar2);
+        progbar3 = Integer.parseInt(sprogressbar3);
+
 
         progressBar1 = (ProgressBar) root.findViewById(R.id.progressBar3);
-        progressBar1.setMax(30);
+        if (progbar1 >= 1){
+            progressBar1.setMax(progbar1);
+        }
+        else{
+            progressBar1.setMax(30);
+        }
         progressBar1.setProgress(0);
         btn5 = (Button) root.findViewById(R.id.button5);
         btn5.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +79,12 @@ public class DashboardFragment extends Fragment {
             }
         });
         progressBar2 = (ProgressBar) root.findViewById(R.id.progressBar4);
-        progressBar2.setMax(30);
+        if (progbar2 >= 1){
+            progressBar2.setMax(progbar2);
+        }
+        else{
+            progressBar2.setMax(30);
+        }
         progressBar2.setProgress(0);
         btn6 = (Button) root.findViewById(R.id.button6);
         btn6.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +97,12 @@ public class DashboardFragment extends Fragment {
             }
         });
         progressBar3 = (ProgressBar) root.findViewById(R.id.progressBar5);
-        progressBar3.setMax(30);
+        if (progbar3 >= 1){
+            progressBar3.setMax(progbar3);
+        }
+        else{
+            progressBar3.setMax(30);
+        }
         progressBar3.setProgress(0);
         btn7 = (Button) root.findViewById(R.id.button7);
         btn7.setOnClickListener(new View.OnClickListener() {
